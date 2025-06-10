5d8651a72ef0502cea7697d91129d3ee69250d9f
Y. Benjamin Perez C. <y.benjamin@ybenpc.com>
2025-06-10 15:53:50 +0100

    Update navigation.tsx

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -61,7 +61,7 @@ export default function Navigation() {
       }`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-8">
+        <div className="flex justify-between items-center h-16">
           <div className="font-bold -ml-4 sm:-ml-6 lg:-ml-32 text-xl text-slate-800">Y. BENJAMIN PEREZ. ENGINEERING PORTFOLIO</div>
 
           {/* Desktop Navigation */}
