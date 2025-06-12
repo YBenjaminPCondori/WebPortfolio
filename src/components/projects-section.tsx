64376388ff505df6a6671b3bb71c6abdab6c457c
Y. Benjamin Perez C. <y.benjamin_pc@hotmail.com>
2025-06-12 21:41:40 +0100

    Commit

--- src/components/projects-section.tsx
+++ src/components/projects-section.tsx
@@ -194,10 +194,8 @@ export default function ProjectsSection() {
 
   return (
     <section
-      id="projects"#
-      className="py-20 bg-gradient-light dark:bg-gradient-dark"
-#
-    >
+      id="projects"
+      className="py-20 bg-gradient-light dark:bg-gradient-dark">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center mb-16">
           <h2 className="text-3xl md:text-4xl font-bold text-black dark:text-blue-500 mb-4">
