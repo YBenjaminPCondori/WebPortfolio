a0742ea7d99d78a8f28cc6310321765f1d7498cb
Y. Benjamin Perez C. <y.benjamin_pc@hotmail.com>
2025-06-15 11:15:23 +0100

    Debugged errors in home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -12,17 +12,16 @@ export default function Home() {
       <Navigation />
       <main>
         <HeroSection />
-        <ArchitectureSection />
-        <ExperienceSection />
-        <ProjectsSection />
         <EducationSection />
+        <ProjectsSection />
+        <ExperienceSection />
         <ContactSection />
       </main>
       <footer className="bg-background text-black dark:text-blue-500 py-8">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 space-y-4 text-center">
           <div className="flex flex-wrap justify-center gap-2">
           </div>
-           <p className="text-black dark:text-blue-500">&copy; Website Developed by (Owner) <b>Y. Benjamin Perez C. © 2025</b></p>
+           <p className="text-black dark:text-blue-500">&copy; Website Developed by (Owner) <b>Y. Benjamin Perez C. © using REACT.js | All Rights Reserved 2025</b></p>
         </div>
       </footer>
     </div>
